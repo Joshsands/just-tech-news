8f3d7f56f09ef77996d87de7f8f7daa6a4ebe56e
Joshua Sands <josh.sands@mail.com>
2021-08-08 15:02:49 -0500

    added voting association to Post and User models

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,18 @@
+const sequelize = require("../../config/connection");
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Vote } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
-    order: [[ 'created_at', 'DESC' ]],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    order: [["created_at", "DESC"]],
     include: [
       {
         model: User,
@@ -25,7 +32,13 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+    [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+  ],
     include: [
       {
         model: User,
@@ -59,6 +72,17 @@ router.post("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// PUT /api/posts/upvote
+router.put("/upvote", (req, res) => {
+  // create the vote
+  Post.upvote(req.body, { Vote })
+      .then((dbPostData) => res.json(dbPostData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
 
 router.put("/:id", (req, res) => {
   Post.update(
