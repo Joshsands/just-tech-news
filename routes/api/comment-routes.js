6a2b6815b8f3497719fb672de4172b8c63b65a30
Joshua Sands <josh.sands@mail.com>
2021-08-10 17:30:15 -0500

    updated comment route

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,11 +2,9 @@ const router = require("express").Router();
 const { Comment } = require("../../models");
 
 router.get("/", (req, res) => {
-  Comment.findAll({
-    attributes: ["id", "comment_text", "user_id", "post_id"],
-  })
-    .then((dbCommentData) => res.json(dbCommentData))
-    .catch((err) => {
+    Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
