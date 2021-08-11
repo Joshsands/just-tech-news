8e1e837d2d2644d2d4a61f40a0b07a95cb25858c
Joshua Sands <josh.sands@mail.com>
2021-08-11 12:36:20 -0500

    added handlebars template and homepage route

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+const path = require('path');
+
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// set handlebars.js as the template engine
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({})
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// express middleware function that can take all of the contens  of a fodler and serve them as static assets
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
